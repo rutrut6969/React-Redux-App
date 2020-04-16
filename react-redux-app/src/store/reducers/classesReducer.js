b5ae37d25e32e9aeebe7f3a99fc66e57c2b96e16
rutrut6969 <rutledgeisaac6969@gmail.com>
2020-04-16 17:01:01 -0500

    Setting up Component Skeleton

--- react-redux-app/src/store/reducers/classesReducer.js
+++ react-redux-app/src/store/reducers/classesReducer.js
@@ -1,37 +1,38 @@
 const initialState = {
-    isFetching: false,
-    count: 0,
-    error: '',
-    classes: {
-        index: '',
-        name: '',
-        url: '',
-    }
-}
+  isFetching: false,
+  count: 0,
+  error: '',
+  classes: {
+    index: '',
+    name: '',
+    url: '',
+  },
+};
 
-export const classesReducer = (state = initialState, action){
-switch(action.type) {
-    case "Classes_Fetching_Start":
-    return {
+export const classesReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'Classes_Fetching_Start':
+      return {
         ...state,
         isFetching: true,
-    };
-    case "Classes_Fetch_Success":
-        return {
-            ...state,
-            isFetching: false,
-            count: state.count + state.classes.length,
-            classes: {
-                index: action.index,
-                name: action.name,
-                url: action.url,
-            }
-        };
-    case "Classes_Fetch_Failure":
-        return{
-            ...state,
-            error: action.error,
-        };
-    default: state;
-}
-}
+      };
+    case 'Classes_Fetch_Success':
+      return {
+        ...state,
+        isFetching: false,
+        count: state.count + state.classes.length,
+        classes: {
+          index: action.index,
+          name: action.name,
+          url: action.url,
+        },
+      };
+    case 'Classes_Fetch_Failure':
+      return {
+        ...state,
+        error: action.error,
+      };
+    default:
+      state;
+  }
+};
